ba003381a3db9e7e6e77c63cedac87e39413a7d7
DavidGWheeler <wots_uh_thedeal@msn.com>
2017-06-28 16:35:54 -0700

    add partial solution

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function openOrSenior(data){
+  let result = new Array;
+  console.log(result);
+  for (let i = 0; i < data.length; i++) {
+    for (let j = 0; j < data[i].length; j++) {
+      if(data[i][j] < 55) {
+        result.push('Open')
+      } else if([i][j + 1] > 7) {
+        result.push('Senior')
+      };
+    }
+  }
+  return result;
+}
